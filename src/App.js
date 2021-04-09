174306db83a3947be508aa1afd8ed32224f6551c
weiya <wave.weiya@gmail.com>
2021-04-09 17:40:47 +0800

    feat: 富文本编辑器 编辑和预览

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from 'react'
+import { Tabs, Button } from 'antd';
+import 'braft-editor/dist/index.css'
+import BraftEditor from 'braft-editor'
+
+import './App.css'
+
+import "antd/dist/antd.css"
+
+const { TabPane } = Tabs;
+
 
 function App() {
+
+  const [keyTab, setTab] = useState("editor")
+
+  /*
+    保存富文本数据
+  */
+  const [editorState, setEditor] = useState(BraftEditor.createEditorState(null))
+
+
+    /*
+    富文本数据 HTML 字符串
+  */
+  const [previewHTML, updatePreviewHTML] = useReducer(()=> {
+    return editorState.toHTML()
+  }, "");
+
+
+  const handleTabChange = (tab) => {
+    setTab(tab)
+    updatePreviewHTML()
+  }
+
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Tabs defaultActiveKey={keyTab} onChange={handleTabChange} type="card">
+
+      <TabPane TabPane tab="editor" key="editor" >
+        <BraftEditor value={editorState} onChange={(editorState) => setEditor(editorState)}/>
+      </TabPane>
+
+      <TabPane tab="preview" key="preview">
+        <div dangerouslySetInnerHTML={{__html: previewHTML}}>
+        </div>
+      </TabPane>
+    </Tabs>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
